e3837fe32cc69e31f25412b6488ba5cd2df2a875
CodingFans78 <codingfans78@hotmail.com>
2024-10-27 19:46:33 -0700

    Add Bar chart.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 The project uses pure Javascript, HTML, and CSS to show data in Charts.
 
+## Charts
+### Column with a Trend Line
+
+### Bar
+
 The data is in JSON format.
 
 ## Examples
-### Bar with Trend Line  
-![Bar with Trend Line](https://github.com/CodingFans78/pure_html_js_charts/blob/main/screen_captures/bar_trend_line.png)
+### Column with Trend Line  
+![Bar with Trend Line](https://github.com/CodingFans78/pure_html_js_charts/blob/main/screen_captures/column_trend_line.png)
+
+![Bar with Trend Line](https://github.com/CodingFans78/pure_html_js_charts/blob/main/screen_captures/bar.png)
--- pure_html_js_charts.js
+++ pure_html_js_charts.js
@@ -1,4 +1,4 @@
-sample_data = {
+sample_data_column = {
     x_axis: {
         values: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
@@ -13,6 +13,18 @@ sample_data = {
     series: {
         values: [200, 300, 400, 300, 200, 300, 500],
         values_2: [60, 30, 40, 30, 20, 30, 65],
+    }
+}
+
+sample_data_bar = {
+    x_axis: {
+        values: [0, 100, 200, 300, 400, 500],
+        unit: ''
+    },
+    series: {
+        items: ['Bar 1', 'Bar 2', 'Bar 3'],
+        values: [250, 500, 400],
+        values_2: [60, 30, 40],
     }
 }
 
@@ -42,9 +54,9 @@ function fillText(ctx, font, text, x, y) {
     ctx.fillText(text, x, y);
 }
 
-// Function: drawBar
-// Input: canvas_id, canvas_width, canvas_height, data.
-function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data) {
+// Function: drawColumn
+// Input: canvas_id, canvas_width, canvas_height, rgba_column, rgba_line, data.
+function drawColumn(wrap_id, canvas_width, canvas_height, rgba_column, rgba_line, data) {
     let canvas = document.getElementById(wrap_id);
     canvas.width = canvas_width;
     canvas.height = canvas_height;
@@ -56,7 +68,7 @@ function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data
         ctx.margin = 44.5;
 
         // X axis
-        drawLine(ctx, 1, ctx.margin, canvas.height - ctx.margin, canvas.width - ctx.margin, ctx.height - ctx.margin, "rgba(0, 0, 0, 1)");
+        drawLine(ctx, 1, ctx.margin, ctx.height - ctx.margin, ctx.width - ctx.margin, ctx.height - ctx.margin, "rgba(0, 0, 0, 1)");
 
         let x_axis_scales = data.x_axis.values.length + 1;
         let x_axis_step = (ctx.width - 2 * ctx.margin) / x_axis_scales;
@@ -64,7 +76,7 @@ function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data
         for (i = 0; i < x_axis_scales; i++) {
             // Draw scale on X axis
             let scale_x = x_axis_step * i + ctx.margin;
-            let scale_y = canvas.height - ctx.margin;
+            let scale_y = ctx.height - ctx.margin;
             drawLine(ctx, 1, scale_x, scale_y, scale_x, scale_y - 10, "rgba(0, 0, 0, 1)");
             
             // Fill text on X axis
@@ -75,7 +87,7 @@ function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data
         }
 
         // Y axis
-        drawLine(ctx, 1, ctx.margin, canvas.height - ctx.margin, ctx.margin, ctx.margin, "rgba(0, 0, 0, 1)")
+        drawLine(ctx, 1, ctx.margin, ctx.height - ctx.margin, ctx.margin, ctx.margin, "rgba(0, 0, 0, 1)");
 
         let y_axis_scales = data.y_axis.values.length - 1;
         let y_axis_step = (ctx.height - 2 * ctx.margin) / y_axis_scales;
@@ -93,47 +105,46 @@ function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data
         }
 
         // Y axis 2
-        drawLine(ctx, 1, canvas.width - ctx.margin, canvas.height - ctx.margin, canvas.width - ctx.margin, ctx.margin, "rgba(0, 0, 0, 0.5)")
+        drawLine(ctx, 1, ctx.width - ctx.margin, ctx.height - ctx.margin, ctx.width - ctx.margin, ctx.margin, "rgba(0, 0, 0, 0.5)")
 
         let y_axis_2_scales = data.y_axis_2.values.length - 1;
         let y_axis_2_step = (ctx.height - 2 * ctx.margin) / y_axis_2_scales;
 
         for (i = 0; i <= y_axis_2_scales; i++) {
             // Draw scale on Y axis 2
-            let scale_x_2 = canvas.width - ctx.margin;
+            let scale_x_2 = ctx.width - ctx.margin;
             let scale_y_2 = ctx.height - ctx.margin - y_axis_2_step * i;
             drawLine(ctx, 1, scale_x_2, scale_y_2, scale_x_2 - 5, scale_y_2, "rgba(0, 0, 0, 0.5)");
 
             // Fill text on Y axis 2
             if (data.y_axis_2.values[i] !== undefined) {
-                fillText(ctx, "10px serif", data.y_axis_2.values[i] + data.y_axis_2.unit, scale_x_2 + ctx.margin / 5, scale_y_2)
+                fillText(ctx, "10px serif", data.y_axis_2.values[i] + data.y_axis_2.unit, scale_x_2 + ctx.margin / 5, scale_y_2);
             }
         }
 
         // Series
         for (i = 0; i < x_axis_scales; i++) {
-            let bar_x = x_axis_step * i + ctx.margin;
-            let bar_y = canvas.height - ctx.margin; 
-            let bar_value = (ctx.height - 2 * ctx.margin) / data.y_axis.values[data.y_axis.values.length - 1] * data.series.values[i]; 
+            let column_x = x_axis_step * i + ctx.margin;
+            let column_y = ctx.height - ctx.margin; 
+            let column_value = (ctx.height - 2 * ctx.margin) / data.y_axis.values[data.y_axis.values.length - 1] * data.series.values[i]; 
 
-            // Bars
+            // Columns
             if (data.series.values[i] !== undefined) {
-                //drawLine(ctx, 50, bar_x, bar_y, bar_x, bar_y - 100)
-                ctx.fillStyle = rgba_bar
-                ctx.fillRect(bar_x - x_axis_step / 3 + x_axis_step, bar_y, x_axis_step * 2 / 3, -bar_value)
+                ctx.fillStyle = rgba_column;
+                ctx.fillRect(column_x - x_axis_step / 3 + x_axis_step, column_y, x_axis_step * 2 / 3, -column_value);
             }
 
             // Value 2 dots
             if (data.series.values_2[i] !== undefined) {
-                let dot_x = bar_x + x_axis_step;
+                let dot_x = column_x + x_axis_step;
                 let dot_y = ctx.height - ctx.margin - (ctx.height - 2 * ctx.margin) / data.y_axis_2.values[data.y_axis_2.values.length - 1] * data.series.values_2[i]; 
                 drawCircle(ctx, dot_x, dot_y, 5, rgba_line);
 
                 // Value 2 trend line
                 if (data.series.values_2[i + 1] !== undefined) {
-                    let dot_x_next = bar_x + 2 * x_axis_step;
+                    let dot_x_next = column_x + 2 * x_axis_step;
                     let dot_y_next = ctx.height - ctx.margin - (ctx.height - 2 * ctx.margin) / data.y_axis_2.values[data.y_axis_2.values.length - 1] * data.series.values_2[i + 1]; 
-                    drawLine(ctx, 2, dot_x, dot_y, dot_x_next, dot_y_next, rgba_line)
+                    drawLine(ctx, 2, dot_x, dot_y, dot_x_next, dot_y_next, rgba_line);
                 }
             }
         }
@@ -141,4 +152,73 @@ function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar, rgba_line, data
     }
 }
 
-drawBar('wrap_bar', 600, 300, "rgba(66, 135, 245, 0.8)", "rgba(0, 160, 0, 1)", sample_data);
+// Function: drawBar
+// Input: canvas_id, canvas_width, canvas_height, rgba_bar_1, rgba_bar_2, data.
+function drawBar(wrap_id, canvas_width, canvas_height, rgba_bar_1, rgba_bar_2, data) {
+    let canvas = document.getElementById(wrap_id);
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+
+    if (canvas.getContext) {
+        let ctx = canvas.getContext("2d");
+        ctx.width = canvas.width;
+        ctx.height = canvas.height;
+        ctx.margin = 44.5;
+
+        // X axis
+        drawLine(ctx, 1, ctx.margin, ctx.height - ctx.margin, ctx.width - ctx.margin, ctx.height - ctx.margin, "rgba(0, 0, 0, 1)");
+
+        let x_axis_scales = data.x_axis.values.length - 1;
+        let x_axis_step = (ctx.width - 2 * ctx.margin) / x_axis_scales;
+
+        for (i = 0; i <= x_axis_scales; i++) {
+            // Draw scale on X axis
+            let scale_x = x_axis_step * i + ctx.margin;
+            let scale_y = ctx.height - ctx.margin;
+            drawLine(ctx, 1, scale_x, scale_y, scale_x, ctx.margin, "rgba(0, 0, 0, 0.3)");
+            
+            // Fill text on X axis
+            if (data.x_axis.values[i] !== undefined) {
+                fillText(ctx, "10px serif", data.x_axis.values[i], scale_x, scale_y + ctx.margin / 2);
+            }
+
+        }
+
+        // Y axis
+        drawLine(ctx, 1, ctx.margin, ctx.height - ctx.margin, ctx.margin, ctx.margin, "rgba(0, 0, 0, 1)")
+
+        let y_axis_scales = data.series.items.length + 1;
+        let y_axis_step = (ctx.height - 2 * ctx.margin) / y_axis_scales;
+
+        for (i = 0; i < y_axis_scales; i++) {
+            // Draw scale on Y axis
+            let scale_x = ctx.margin;
+            let scale_y = ctx.height - ctx.margin - y_axis_step * i;
+            drawLine(ctx, 1, scale_x, scale_y, ctx.margin + 5, scale_y, "rgba(0, 0, 0, 1)");
+
+            // Fill text on Y axis
+            if (data.series.items[i - 1] !== undefined) {
+                fillText(ctx, "10px serif", data.series.items[i - 1], scale_x - ctx.margin / 2, scale_y);
+            }
+        }
+
+        // Series
+        for (i = 0; i < y_axis_scales; i++) {
+            let bar_x = ctx.margin;
+            let bar_y = ctx.height - ctx.margin - y_axis_step * (i + 1);
+            console.log('data.x_axis.values[data.x_axis.values.length - 1]: ', data.x_axis.values[data.x_axis.values.length - 1]);
+            console.log(' data.series.values[i]: ',  data.series.values[i]);
+            let bar_value = (ctx.width - 2 * ctx.margin) / data.x_axis.values[data.x_axis.values.length - 1] * data.series.values[i] + ctx.margin; 
+        
+
+            // Bars
+            if (data.series.values[i] !== undefined) {
+                drawLine(ctx, 30, bar_x, bar_y, bar_value, bar_y, rgba_bar_1); 
+            }
+        }
+    }
+}
+
+drawColumn('wrap_column', 600, 300, "rgba(66, 135, 245, 0.8)", "rgba(0, 160, 0, 1)", sample_data_column);
+
+drawBar('wrap_bar', 600, 300, "rgba(66, 135, 245, 0.8)", "rgba(0, 160, 0, 1)", sample_data_bar);
